52be97e0dca776841a7bc789f54e305d10caa26e
Greg-Rus <grzegorz.rus@gmail.com>
2014-07-07 17:42:02 +0200

    First atempt at raising memory cells
    Instead of spwawning memCells I now spawn a memoryArray - a long
    memCell. The sinWave generator now iterates over the arrays once.
    First attempt a raising memCells:
    spawn a fixed number of memCells arround the player. keep spawning new
    rows as the player moves. Destroy memCells that exit the area colider.

---
+++ Emergence/Assets/Scripts/DieOnCollisionExit.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DieOnCollisionExit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerExit() {
+		Destroy (this.gameObject);
+		print ("Destroy!!");
+	} 
+}
---
+++ Emergence/Assets/Scripts/MemArrayController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MemArrayController : MonoBehaviour {
+	public GameObject memArray;
+	private GameObject [] memIndex;
+	private int i;
+	public int xSize;
+	private float iVector;
+	public float cellSideSize;
+	private Vector3 spawnSpot;
+	private float counter = 0f;
+	private Vector3 dYposition;
+	private float defElevation = 0.2f;
+	private float lastCounter;
+
+	// Use this for initialization
+	void Start () {
+		cellSideSize = 1.0f;
+
+		memIndex = new GameObject[xSize];
+		for (i = 0; i<xSize; i++) {
+						iVector = (float)i * cellSideSize;
+		
+						spawnSpot = new Vector3 (iVector, -0.9f, 0.0f);
+
+						memIndex[i] = Instantiate (memArray, spawnSpot, Quaternion.identity) as GameObject;
+				}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//counter = 0f;
+		foreach (GameObject item in memIndex){
+
+			dYposition = item.transform.position;
+			dYposition.y = -0.9f + defElevation * sinWave();
+			
+			item.transform.position = dYposition;
+			counter = counter + 1.0f;
+		}
+		lastCounter = lastCounter+0.01f;
+		counter = lastCounter;
+	}
+
+	float sinWave(){
+		float val = counter;
+		if (val >= Mathf.PI*2)	val -= Mathf.PI*2 ;
+		val = Mathf.Sin (val*1)*1;
+		val += Time.deltaTime;
+		
+		return val;
+	}
+
+}
