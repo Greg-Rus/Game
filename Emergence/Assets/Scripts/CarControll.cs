5ad352d0c130b7ae3181c5b75be3dacc03b5f638
Greg-Rus <grzegorz.rus@gmail.com>
2014-09-05 23:11:20 +0200

    Car Physics
    Need to fix wheel turn quaternion creation. Need to add slipping. Need
    to fix braking.

--- Emergence/Assets/Scripts/CarControll.cs
+++ Emergence/Assets/Scripts/CarControll.cs
@@ -8,6 +8,7 @@ public class CarControll : MonoBehaviour {
 	public WheelCollider wheelFL;
 	public WheelCollider wheelML;
 	public WheelCollider wheelBL;
+	public Transform wheelParent;
 
 	public Transform wheelTransformFR;
 	public Transform wheelTransformMR;
@@ -18,40 +19,137 @@ public class CarControll : MonoBehaviour {
 
 	public float maxTorque = 20;
 	public float maxSteerAngle = 10;
+	public float maxBrakeTorque = 50;
+	public float maxSpeed = 50;
 	private float currentTorque;
 	private float currentSteerAngle;
+	private float lastSteerAngle;
+	private float deltaOfSteerAngle;
 	private Vector3 fixedCenterOfMass;
+	private bool isBraking;
+	public float currentSpeed;
+	private float deltaTime;
+
+
 	// Use this for initialization
 	void Start () {
+		isBraking = false;
 		fixedCenterOfMass = rigidbody.centerOfMass;
 		fixedCenterOfMass.y = -0.7f;
 		rigidbody.centerOfMass = fixedCenterOfMass;
+		lastSteerAngle = currentSteerAngle;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+		currentSpeed = (Mathf.PI * 2 * wheelFR.radius) * wheelFR.rpm *0.06f;
 		currentTorque = maxTorque * Input.GetAxis ("Vertical");
 		currentSteerAngle = maxSteerAngle * Input.GetAxis ("Horizontal");
+		ApplyTorqueToWheels ();
+		ApplySteeringAngleToWheels ();
 
-		wheelBR.motorTorque = currentTorque;
-		wheelBL.motorTorque = currentTorque;
 
-		wheelFR.steerAngle = currentSteerAngle;
-		wheelFL.steerAngle = currentSteerAngle;
+
 	}
 
 	void Update() {
-		wheelTransformFR.localEulerAngles = new Vector3 (0f, wheelFR.steerAngle, 0f );
-		wheelTransformFL.localEulerAngles = new Vector3 (0f, wheelFL.steerAngle, 0f );
+
+		RotateWheels ();
+		TurnSteeringWheels ();
 
 
-		wheelTransformFR.Rotate (wheelFR.rpm * -6f *Time.deltaTime, 0, 0);
-		wheelTransformMR.Rotate (wheelMR.rpm * -6f *Time.deltaTime, 0, 0);
-		wheelTransformBR.Rotate (wheelBR.rpm * -6f *Time.deltaTime, 0, 0);
-		wheelTransformFL.Rotate (wheelFL.rpm * -6f *Time.deltaTime, 0, 0);
-		wheelTransformML.Rotate (wheelML.rpm * -6f *Time.deltaTime, 0, 0);
-		wheelTransformBL.Rotate (wheelBL.rpm * -6f *Time.deltaTime, 0, 0);
+//		lastSteerAngle = lastSteerAngle - currentSteerAngle;
+//		float FL = wheelFL.steerAngle - wheelTransformFL.localEulerAngles.z;
+//		float FR = wheelFR.steerAngle - wheelTransformFR.localEulerAngles.z;
+//		wheelTransformFL.localEulerAngles = new Vector3 (wheelTransformFL.localEulerAngles.x - wheelTransformFL.localEulerAngles.z, wheelFL.steerAngle, wheelTransformFL.localEulerAngles.z);
+//		wheelTransformFR.localEulerAngles = new Vector3 (wheelTransformFR.localEulerAngles.x - wheelTransformFR.localEulerAngles.z, wheelFR.steerAngle, wheelTransformFR.localEulerAngles.z);
+//		wheelTransformFL.Rotate (0, lastSteerAngle - currentSteerAngle, 0);
+//		wheelTransformFR.Rotate (0, lastSteerAngle - currentSteerAngle, 0);
+//		WheelSuspensionUpdate ();
+
 
 
 	}
+
+	void ApplyTorqueToWheels(){
+	
+		if (((currentSpeed> 0) && (Input.GetAxis("Vertical") <0 )) || ((currentSpeed< 0) && (Input.GetAxis("Vertical") > 0 ))){
+			isBraking = true;
+		}
+		else {
+			isBraking = false;
+			wheelFL.brakeTorque =0;
+			wheelFR.brakeTorque =0;
+		}
+		
+		if (isBraking ==false) {
+			if (currentSpeed < maxSpeed){
+				wheelBR.motorTorque = currentTorque;
+				wheelBL.motorTorque = currentTorque;
+				wheelMR.motorTorque = currentTorque;
+				wheelML.motorTorque = currentTorque;
+			}
+			else {
+				wheelBR.motorTorque = 0;
+				wheelBL.motorTorque = 0;
+				wheelMR.motorTorque = 0;
+				wheelML.motorTorque = 0;
+			}
+		}
+		else {
+			wheelFL.brakeTorque = maxBrakeTorque;
+			wheelFR.brakeTorque = maxBrakeTorque;
+			wheelBR.motorTorque = 0;
+			wheelBL.motorTorque = 0;
+			wheelMR.motorTorque = 0;
+			wheelML.motorTorque = 0;
+		}
+		
+
+
+	}
+
+	void ApplySteeringAngleToWheels (){
+		wheelFR.steerAngle = currentSteerAngle;
+		wheelFL.steerAngle = currentSteerAngle;
+
+	}
+
+	void RotateWheels(){
+		float deltaTime = Time.fixedDeltaTime;
+
+//		wheelTransformFR.Rotate (wheelFR.rpm * 6f *deltaTime, 0, 0);
+//		wheelTransformFL.Rotate (wheelFL.rpm * 6f *deltaTime, 0, 0);
+		
+		wheelTransformMR.Rotate (wheelMR.rpm * 6f *deltaTime, 0, 0);
+		wheelTransformBR.Rotate (wheelBR.rpm * 6f *deltaTime, 0, 0);
+		
+		wheelTransformML.Rotate (wheelML.rpm * 6f *deltaTime, 0, 0);
+		wheelTransformBL.Rotate (wheelBL.rpm * 6f *deltaTime, 0, 0);
+	}
+
+	void TurnSteeringWheels() {
+//		deltaOfSteerAngle = currentSteerAngle - lastSteerAngle;
+		
+//		wheelTransformFL.RotateAround (wheelFL.transform.position, Vector3.up, deltaOfSteerAngle );
+//		wheelTransformFR.RotateAround (wheelFR.transform.position, Vector3.up, deltaOfSteerAngle );
+		deltaTime = Time.fixedDeltaTime;
+
+		wheelTransformFL.localRotation = Quaternion.Euler (
+
+			Mathf.Repeat (wheelTransformFL.localRotation.eulerAngles.x + deltaTime * wheelFL.rpm * 6f, 360.0f),
+			wheelFL.steerAngle,
+			0f);
+		Debug.Log (Mathf.Repeat (wheelTransformFL.localRotation.eulerAngles.x + deltaTime * wheelFL.rpm * 6f, 360.0f));
+		wheelTransformFR.localRotation = Quaternion.Euler (
+			Mathf.Repeat (wheelTransformFR.localRotation.eulerAngles.x + deltaTime * wheelFR.rpm * 6f, 360.0f),
+			wheelFR.steerAngle,
+			wheelTransformFL.localRotation.eulerAngles.z);
+//		lastSteerAngle = currentSteerAngle;
+
+	}
+
+
+
+
 }
