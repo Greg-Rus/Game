4f26915a7571165e307f1f1aec5dc52a6b5e95bb
Greg-Rus <grzegorz.rus@gmail.com>
2015-01-07 15:23:46 +0100

    FSM Framework Started
    Can't decide if I should make it abstract or not

---
+++ Emergence/Assets/Scripts/AI Scripts/StateClasses.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartPatrolState : FSMState
+{
+	private Transform[] patrolWaypoints;
+	private int closestWaypointIndex;
+	private Transform closestWaypoint;
+	
+	public StartPatrolState(Transform[] waypoints)
+	{
+		patrolWaypoints = waypoints;
+		stateID = StateID.StartPatrol;
+	}
+	
+	public override void Reason(GameObject player, GameObject npc)
+	{
+		if(closestWaypointIndex != -1)
+		{
+			
+			closestWaypointIndex = -1;
+		}
+	}
+	
+	public override void Act (GameObject player, GameObject npc)
+	{
+		Transform closestWaypoint = patrolWaypoints [0];
+		closestWaypointIndex = 0;
+		
+		float distanceToClosest = (patrolWaypoints [0].position - npc.transform.position).magnitude;
+		for (int i=1; i<patrolWaypoints.Length; i++) 
+		{
+			float distanceToWaypoint = (patrolWaypoints[i].position - npc.transform.position).magnitude;	
+			if (distanceToWaypoint <= distanceToClosest)
+			{
+				closestWaypointIndex = i;
+				distanceToClosest = distanceToWaypoint;
+			}
+			
+		}
+		npc.GetComponent<TankController>().currentWaypoint = closestWaypointIndex;
+		npc.GetComponent<TankController>().SetTransition(Transition.foundClosestWaypoint);
+	}
+}
+
+public class PatrollingState : FSMState
+{
+	private Transform[] patrollWaypoints;
+	
+	public PatrollingState(Transform[] waypoints)
+	{
+		patrollWaypoints = waypoints;
+	}
+	
+	public override void Reason (GameObject PoI, GameObject NPC)
+	{
+	
+	}
+	
+	public override void Act (GameObject PoI, GameObject NPC)
+	{
+		
+	}
+}
---
+++ Emergence/Assets/Scripts/AI Scripts/TankController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankController : MonoBehaviour {
+	public GameObject Player;
+	public Transform[] patrolWaypoints;
+	public int currentWaypoint;
+	private FSMSystem fsm;
+	// Use this for initialization
+	public void SetTransition(Transition t) { fsm.PerformTransition(t); } //setter to use private fsm?
+	void Start () {
+		MakeFSM ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		fsm.CurrentState.Reason(Player, gameObject);
+		fsm.CurrentState.Act(Player, gameObject);
+	}
+
+	private void MakeFSM()
+	{
+		StartPatrolState startPatrol = new StartPatrolState (patrolWaypoints);
+		startPatrol.AddTransition(Transition.foundClosestWaypoint , StateID.Patroling);
+	}
+}
+
+
