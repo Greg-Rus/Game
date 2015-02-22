39fa7d1cc970bd82ea56e31fafa6ff4d429ce49d
Greg-Rus <grzegorz.rus@gmail.com>
2015-02-22 19:26:33 +0100

    Modified eplosion hadling for projectile

--- Emergence/Assets/Scripts/Arena Scripts/Explode.cs
+++ Emergence/Assets/Scripts/Arena Scripts/Explode.cs
@@ -23,6 +23,35 @@ public class Explode : MonoBehaviour {
 			if (col.rigidbody)
 
 			{
+				//Debug.Log("Hit Layer" + col.gameObject.layer);
+				switch (col.gameObject.layer){
+				//swith
+				case 12:
+					if(script = col.rigidbody.GetComponent("TankMinionMobility") as TankMinionMobility) {
+						script.usePhysics();
+					}
+					simpleExplode(col);
+					script = null;
+					deductHealth(col);
+					break;
+				//framework
+				case 13:
+					if(F_script = col.rigidbody.GetComponent<F_ControlMode>()) {
+						F_script.usePhysics();
+					}
+					simpleExplode(col);
+					F_script = null;
+					deductHealth(col);
+					break;				
+				//RAIN
+				case 14:
+					deductHealth(col);
+					break;
+				default: 
+					simpleExplode(col);
+					break;
+				}
+/*				
 				if(col.rigidbody.isKinematic)
 				{
 					if(script = col.rigidbody.GetComponent("TankMinionMobility") as TankMinionMobility) {
@@ -56,9 +85,53 @@ public class Explode : MonoBehaviour {
 				if (NPCStats = col.GetComponent(typeof(F_Stats)) as F_Stats){
 					NPCStats.takeDamage(20f);
 				}
+*/
 			}
 		}
 		Destroy(gameObject); // destroy the grenade
 		Destroy(expl, 3); // delete the explosion after 3 seconds
+	}
+	
+	void simpleExplode(Collider col)
+	{
+		col.rigidbody.AddExplosionForce (explosionPower,
+		                                 transform.position,
+		                                 explosionRadius,
+		                                 1f,
+		                                 ForceMode.Impulse);
+	}
+	
+	void damagingExplode(Collider col)
+	{
+		if(col.rigidbody.isKinematic)
+		{
+			if(script = col.rigidbody.GetComponent<TankMinionMobility>()) {
+				script.usePhysics();
+			}
+			script = null;
+			
+			if(F_script = col.rigidbody.GetComponent<F_ControlMode>()) {
+				F_script.usePhysics();
+			}
+			F_script = null;
+			
+		}
+	}
+	
+	void deductHealth(Collider col)
+	{
+		if (healthManager = col.GetComponent<Health>()){
+			healthManager.takeDamage(20f);
+		}
+		else if (NPCStats = col.GetComponent(typeof(F_Stats)) as F_Stats){
+			NPCStats.takeDamage(20f);
+		}
+		else if (tRig = col.rigidbody.GetComponentInChildren<AIRig>())
+			{
+				Debug.Log (tRig);
+				float currentHelath = tRig.AI.WorkingMemory.GetItem<float>("health");
+				tRig.AI.WorkingMemory.SetItem<float>("health", currentHelath - 20f);
+			}
+		
 	}
 }
